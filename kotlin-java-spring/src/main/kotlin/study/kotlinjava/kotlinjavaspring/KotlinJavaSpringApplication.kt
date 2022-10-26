af6c3a3458a69c47925eb06220197a1fe85b2d26
홍준성 <ghdwnstjd13@gmail.com>
2022-10-26 14:17:09 +0900

    New Module > kotlin-java-spring

---
+++ kotlin-java-spring/src/main/kotlin/study/kotlinjava/kotlinjavaspring/KotlinJavaSpringApplication.kt
@@ -0,0 +1,11 @@
+package study.kotlinjava.kotlinjavaspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJavaSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinJavaSpringApplication>(*args)
+}
---
+++ kotlin-java-spring/src/test/kotlin/study/kotlinjava/kotlinjavaspring/KotlinJavaSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlinjava.kotlinjavaspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJavaSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
